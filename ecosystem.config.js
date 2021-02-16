da921809e11c7a26c4d1557c14024f9b28cc7688
Nam Vo <vhnam2504@gmail.com>
2021-02-16 19:49:02 +0700

    chore: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,14 +6,10 @@ module.exports = {
       args: 'start',
       env: {
         NODE_ENV: 'development',
-        HOST: '0.0.0.0',
-        PORT: 3000,
         API_PATH: 'http://localhost:1337',
       },
       env_production: {
         NODE_ENV: 'production',
-        HOST: '0.0.0.0',
-        PORT: 13000,
         API_PATH: 'http://fake-udigital-cms.sestud.io',
       },
     },
